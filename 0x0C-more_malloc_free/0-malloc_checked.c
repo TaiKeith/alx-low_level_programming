1c5032094ca8c20bce2e4fcf2c0b3c16310f1934
Tai Keith <keithsteve.ks@hotmail.com>
2023-03-16 09:44:04 -0700

    0-malloc_checked.c

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * malloc_checked - Allocate memory using malloc
+ * Description: If malloc fails, terminate process with status 98
+ * @b: unsigned int memory size to allocate
+ * Return: void pointer to allocated memory space
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *k;
+
+	k = malloc(b);
+	if (k == NULL)
+		exit(98);
+
+	return (k);
+}
