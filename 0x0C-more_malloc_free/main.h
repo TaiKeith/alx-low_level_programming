c35dcf218887f27252480cd68dbe2b2735bb9aad
Tai Keith <keithsteve.ks@hotmail.com>
2023-03-16 12:04:22 -0700

    101-mul.c

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -5,3 +5,4 @@ void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size,unsigned int new_size);
 int main(int argc, char **argv);
+int _atoi(char *s);
