7c9412b321615850321c3450886b2c75ac382a30
Tai Keith <keithsteve.ks@hotmail.com>
2023-03-16 12:27:51 -0700

    101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -9,7 +9,7 @@
  */
 int main(int argc, char **argv)
 {
-	int num1, num2, mul;
+	int num1, num2, result;
 
 	if (argc != 3)
 	{
@@ -20,7 +20,7 @@ int main(int argc, char **argv)
 	{
 		num1 = atoi(argv[1]);
 		num2 = atoi(argv[2]);
-		mul = num1 * num2;
+		result = num1 * num2;
 		printf("%d\n", result);
 		return (0);
 	}
