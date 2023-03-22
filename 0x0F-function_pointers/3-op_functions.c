bc646c6585f198f745d08bd4f267317e677d3c75
Tai Keith <keithsteve.ks@hotmail.com>
2023-03-22 11:38:39 -0700

    3-main.c, 3-op_functions.c, 3-get_op_func.c, 3-calc.h

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,11 +1,12 @@
+#include "3-calc.h"
 #include <stdlib.h>
 #include <stdio.h>
 
 /**
- * op_add - A function that returns the sum of a and b
- * @a: 1st input integer
- * @b: 2nd input integer
- * Return: Result to sum a + b
+ * op_add - a function that returns the sum of a and b.
+ * @a: an input integer
+ * @b: an input integer
+ * Return: The result to sum a + b.
  */
 int op_add(int a, int b)
 {
@@ -13,10 +14,10 @@ int op_add(int a, int b)
 }
 
 /**
- * op_sub - A function that returns the subtract of a and b
- * @a: 1st input integer
- * @b: 2nd input integer
- * Return: Result to sum a - b
+ * op_sub - a function that returns the subtract of a and b.
+ * @a: an input integer
+ * @b: an input integer
+ * Return: The result to substract a - b.
  */
 int op_sub(int a, int b)
 {
@@ -24,10 +25,10 @@ int op_sub(int a, int b)
 }
 
 /**
- * op_mul - A function that returns the product of a and b
- * @a: 1st input integer
- * @b: 2nd input integer
- * Return: Result to sum a * b
+ * op_mul - a function that returns the multiply of a and b.
+ * @a: an input integer
+ * @b: an input integer
+ * Return: The result to multiply a * b.
  */
 int op_mul(int a, int b)
 {
@@ -35,26 +36,27 @@ int op_mul(int a, int b)
 }
 
 /**
- * op_div - A function that returns the reult of division of a and b
- * @a: 1st input integer
- * @b: 2nd input integer
- * Return: Result to sum a / b
+ * op_div - a function that returns the divide of a and b.
+ * @a: an input integer
+ * @b: an input integer
+ * Return: The result to divide a / b.
  */
 int op_div(int a, int b)
 {
 	if (b == 0)
 	{
-		printf("Error\n")
+		printf("Error\n");
 		exit(100);
 	}
+
 	return (a / b);
 }
 
 /**
- * op_mod - A function that returns the remainder of division of a and b
- * @a: 1st input integer
- * @b: 2nd input integer
- * Return: Result to sum a % b
+ * op_mod - a function that returns the module of a and b.
+ * @a: an input integer
+ * @b: an input integer
+ * Return: The result to module a % b.
  */
 int op_mod(int a, int b)
 {
