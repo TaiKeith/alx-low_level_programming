6f1df6073eff93859d8c1bda57c630ecb8280467
Tai Keith <keithsteve.ks@hotmail.com>
2023-03-22 08:07:42 -0700

    0-print_name.c

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,1 +1,13 @@
+#ifndef _FUNCTION_POINTERS_H_
+#define _FUNCTION_POINTERS_H_
+
+/**
+ * Desc: This is a header file containing prototypes for all functions
+ * 	 used in the 0x0E-function_pointers directory.
+ */
+
+int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
+
+
+#endif
