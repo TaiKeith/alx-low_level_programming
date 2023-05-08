180dc37d27d39e44dcd49b58d35d693d5721a838
Tai Keith <keithsteve.ks@hotmail.com>
2023-05-08 03:27:49 -0700

    100-elf_header.c

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,4 +1,9 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <elf.h>
 
 /**
  * main - The entry point for program to get header of ELF file
