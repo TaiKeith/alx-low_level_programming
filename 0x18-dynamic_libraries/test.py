a5ec0ada6894d796208cd270298ae686fa30ef1c
Tai Keith <keithsteve.ks@hotmail.com>
2023-08-21 06:24:18 -0700

    Update

--- 0x18-dynamic_libraries/test.py
+++ 0x18-dynamic_libraries/test.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 import ctypes
+import random
 
 lib = ctypes.CDLL("./100-operations.so")
-result = lib.mul(4, 7)
+a = random.randint(-111, 111)
+b = random.randint(-111, 111)
+result = lib.mul(a, b)
 
-print(f"The result of multiplication is {result}")
+print(f"The result of multiplying {a} and {b} is {result}")
