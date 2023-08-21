acff1dbce7b7799ac5659ef95d4ad9db7fdf1600
Tai Keith <keithsteve.ks@hotmail.com>
2023-08-21 06:49:40 -0700

    Updated checks

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+
+/**
+ * add - A function that adds two integers
+ * @a: integer to be added
+ * @b: integer to be added
+ *
+ * Return: The result after adding the two integers
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - A function that subtracts two integers
+ * @a: integer to be subtracted
+ * @b: integer to be subtracted
+ *
+ * Return: The result after subtracting the two integers
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - A function that multiplies two integers
+ * @a: integer to be multiplied
+ * @b: integer to be multiplied
+ *
+ * Return: The result after multiplying the two integers
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - A function that divides two integers
+ * @a: integer to be divided
+ * @b: integer to be divided
+ *
+ * Return: The result after dividing the two integers
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - A function that returns the modulus of two integers
+ * @a: integer
+ * @b: integer
+ *
+ * Return: The result after modulus operation on the two integers
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
