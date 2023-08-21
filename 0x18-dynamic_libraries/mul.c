72a0f9ee6819d04e8da77d26305e33600abe707a
Tai Keith <keithsteve.ks@hotmail.com>
2023-08-21 06:09:07 -0700

    A dynamic library that contains C functions that can be called from Python

---
+++ 0x18-dynamic_libraries/mul.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * mul - A function that multiplies two integers
+ * @a: integer to be multiplied
+ * @b: integer to be multiplied
+ *
+ * Return: The result after multiplying the two integers
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
