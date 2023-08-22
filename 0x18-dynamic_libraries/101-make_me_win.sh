684af920cb6862de9db09bf374f0e8b6a2e05bcc
Tai Keith <keithsteve.ks@hotmail.com>
2023-08-21 17:33:34 -0700

    Jackpot, you win

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/TaiKeith/alx-low_level_programming/raw/master/0x18-dynamic_libraries/mylibmytest.so
+wget -P /tmp https://github.com/TaiKeith/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libmytest.so
 export LD_PRELOAD=/tmp/libmytest.so
