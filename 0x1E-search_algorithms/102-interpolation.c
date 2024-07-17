2f0b1e5a1eaf86321c65644279f08d978b19190b
Tai Keith <keithsteve.ks@hotmail.com>
2024-07-17 14:31:13 -0700

    Fixed Interpolation search

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -1,7 +1,7 @@
 #include "search_algos.h"
 
 /**
- * interpolation_search - A function that searches for a value in a sorted array
+ * interpolation_search - Function that searches for a value in a sorted array
  * @array: Pointer to the first element of an array
  * @size: Number of elements in an array
  * @value: Value to search for
